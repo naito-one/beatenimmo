3618465843a49cfad348e7bee478139a3262379d
Tim Winkelmann <twinkelmann@protonmail.com>
2025-04-10 00:28:34 +0200

    Fixed use of useRoute outside composable

--- composables/useQuery.ts
+++ composables/useQuery.ts
@@ -1,11 +1,10 @@
 import type { LocationQuery } from 'vue-router'
-// declare outside of the function so everyone uses the same hook
-const route = useRoute()
-const router = useRouter()
-const query: Ref<LocationQuery> = ref(route.query)
-router.afterEach((route) => {
-  query.value = route.query
-})
 export function useQuery() {
+  const route = useRoute()
+  const router = useRouter()
+  const query: Ref<LocationQuery> = ref(route.query)
+  router.afterEach((route) => {
+    query.value = route.query
+  })
   return query
 }

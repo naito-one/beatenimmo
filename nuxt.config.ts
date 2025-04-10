96e2c8a3f353c8c1810387bb3ed4ade5c3940d87
Tim Winkelmann <twinkelmann@protonmail.com>
2025-04-10 05:46:57 +0200

    Set proper base URL for prod

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -120,7 +120,7 @@ export default defineNuxtConfig({
   },
 
   i18n: {
-    baseUrl: 'https://beatenimmo.nuxt.dev/', // https://beatenimmo.ch
+    baseUrl: 'https://beatenimmo.ch',
     strategy: 'prefix_except_default',
     defaultLocale: 'en',
     locales: [
